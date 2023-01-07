2ce26de57d3714ce32feb689624f68c5b61e0408
damiansahuan <damiansahuan@hotmail.com>
2023-01-07 20:09:09 -0300

    git status

--- js/main.js
+++ js/main.js
@@ -5,18 +5,31 @@ let conocimiento
 let habilidad
 let actitud
 let valor
+let cantidadPersonas
 
 
-//pedir el nombre de la primera persona a valorar
-nombrePersona = perseInt(prompt("Ingrese el nombre de la persona a valorar"))
+//pedir cantidad de personas a valorar
+cantidadPersonas = parseInt(prompt(`Ingrese la cantidad de personas a valorar`))
 
 
+//pedir nombre y valores sobre esa personas
+//nombrePersona = prompt("Ingrese el NOMBRE de la persona a valorar")
 
-//pedir valores sobre esa personas
+//conocimiento = parseInt(prompt(`del 1 al 10, ingrese el CONOCIMIENTO de  ${nombrePersona}`))
+//habilidad = parseInt(prompt(`del 1 al 10, ingrese la HABILIDAD de ${nombrePersona}`))
+//valor = conocimiento + habilidad
+//actitud = parseInt(prompt(`del 1 al 10, ingrese la ACITUD de ${nombrePersona}`))
+//valor = (valor * actitud) / 2
+//console.log(`El VALOR de ${nombrePersona} es ${valor}`)
 
-conocimiento = parseInt(prompt("del 1 al 10, ingrese el conocimiento de + nombrePersona"))
-habilidad = parseInt(prompt("del 1 al 10, ingrese la habilidad de + nombrePersona"))
-valor = conocimiento + habilidad
-actitud = parseInt(prompt("del 1 al 10, ingrese el conocimiento de + nombrePersona"))
-valor = valor * actitud
-console.log(El VALOR de + nombrePersona es + valor)
+//Codigo funcionando
+for(let i = 1; i <=cantidadPersonas; i++ ){
+    nombrePersona = prompt("Ingrese el NOMBRE de la persona a valorar")
+
+    conocimiento = parseInt(prompt(`del 1 al 10, ingrese el CONOCIMIENTO de  ${nombrePersona}`))
+    habilidad = parseInt(prompt(`del 1 al 10, ingrese la HABILIDAD de ${nombrePersona}`))
+    valor = conocimiento + habilidad
+    actitud = parseInt(prompt(`del 1 al 10, ingrese la ACITUD de ${nombrePersona}`))
+    valor = (valor * actitud) / 2
+    console.log(`El VALOR de ${nombrePersona} es ${valor}`)
+}
